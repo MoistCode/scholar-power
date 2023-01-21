33aea1ad0ebd102521c1d75af0263cd14e612bc2
moistcode <tpham@smugmug.com>
2023-01-21 11:48:03 -0800

    add signup page

---
+++ src/pages/SignUp.tsx
@@ -0,0 +1,46 @@
+import { IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonList, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { useRef } from 'react';
+
+const SignUp: React.FC = () => {
+  let usernameInputRef = useRef<HTMLIonInputElement>(null);
+  let passwordInputRef = useRef<HTMLIonInputElement>(null);
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Sign Up</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonList>
+          <IonItem>
+            <IonLabel>Username</IonLabel>
+            <IonInput
+              ref={usernameInputRef}
+              placeholder="Enter username"
+            />
+          </IonItem>
+          <IonItem>
+            <IonLabel>Password</IonLabel>
+            <IonInput
+              ref={passwordInputRef}
+              placeholder="Enter password"
+              type="password"
+            />
+          </IonItem>
+        </IonList>
+        <IonToolbar>
+          <IonButtons slot="primary">
+            <IonButton fill="outline">
+              <IonIcon slot="primary"/>
+              Sign Up
+            </IonButton>
+          </IonButtons>
+        </IonToolbar>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default SignUp;
