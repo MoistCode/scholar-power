9b923abd47884476223d1ae5faa61e30ea4420c7
moistcode <tpham@smugmug.com>
2023-01-22 18:14:36 -0800

    Add suppose for non-functional user button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,25 @@
-import { IonHeader, IonToolbar, IonTitle } from "@ionic/react";
+import { IonHeader, IonToolbar, IonTitle, IonButton, IonButtons, IonIcon } from "@ionic/react";
+import { personCircle } from "ionicons/icons";
+import useToken from "../hooks/useToken";
 
 const Header = (props: HeaderProps) => {
   let {
     title,
   } = props;
 
+  let { token } = useToken();
+
   return (
     <IonHeader>
       <IonToolbar>
         <IonTitle>{title}</IonTitle>
+        {token &&
+          <IonButtons slot="secondary">
+            <IonButton>
+              <IonIcon slot="icon-only" icon={personCircle}></IonIcon>
+            </IonButton>
+          </IonButtons>
+        }
       </IonToolbar>
     </IonHeader>
   );
