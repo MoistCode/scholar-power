9c4d53b04a360326f6a0ef06e4bc51a5a1827803
moistcode <tpham@smugmug.com>
2023-01-22 18:54:11 -0800

    Example of list of workouts

--- src/pages/Workouts.tsx
+++ src/pages/Workouts.tsx
@@ -1,5 +1,51 @@
-import { IonContent, IonPage } from '@ionic/react';
+import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonIcon, IonPage } from '@ionic/react';
+import { createOutline } from 'ionicons/icons';
 import Header from '../components/Header';
+import styles from './Workouts.module.css';
+
+// TODO: Replace this with a call to the API.
+const exampleListOfWorkouts = [
+  {
+    title: "Workout 1",
+    subtitle: "Workout 1 subtitle",
+    description: "Workout 1 description",
+  },
+  {
+    title: "Workout 2",
+    subtitle: "Workout 2 subtitle",
+    description: "Workout 2 description",
+  },
+  {
+    title: "Workout 3",
+    subtitle: "Workout 3 subtitle",
+    description: "Workout 3 description",
+  },
+  {
+    title: "Workout 4",
+    subtitle: "Workout 4 subtitle",
+    description: "Workout 4 description",
+  },
+  {
+    title: "Workout 5",
+    subtitle: "Workout 5 subtitle",
+    description: "Workout 5 description",
+  },
+  {
+    title: "Workout 6",
+    subtitle: "Workout 6 subtitle",
+    description: "Workout 6 description",
+  },
+  {
+    title: "Workout 7",
+    subtitle: "Workout 7 subtitle",
+    description: "Workout 7 description",
+  },
+  {
+    title: "Workout 8",
+    subtitle: "Workout 8 subtitle",
+    description: "Workout 8 description",
+  }
+];
 
 /**
  * This component is the page responsible for displaying the user-generated
@@ -10,10 +56,51 @@ const Workouts = () => {
     <IonPage>
       <Header title="Workouts" />
       <IonContent fullscreen>
-        Workouts page example
+        {exampleListOfWorkouts.map((workout, index) => {
+          return (
+            <WorkoutCard
+              key={`workout.title-${index}`}
+              title={workout.title}
+              subtitle={workout.subtitle}
+              description={workout.description}
+            />
+          );
+        })}
       </IonContent>
     </IonPage>
   );
 };
 
 export default Workouts;
+
+const WorkoutCard = (props: WorkoutCardProps) => {
+  let {
+    title,
+    subtitle,
+    description,
+  } = props;
+
+  return (
+    <IonCard>
+      <IonCardHeader>
+        <IonCardTitle>{title}</IonCardTitle>
+        <IonCardSubtitle>{subtitle}</IonCardSubtitle>
+      </IonCardHeader>
+
+      <IonCardContent>
+        {description}
+      </IonCardContent>
+      <div className={styles.createButtonContainer}>
+        <IonButton fill="clear" >
+          <IonIcon slot="icon-only" icon={createOutline} />
+        </IonButton>
+      </div>
+    </IonCard>
+  );
+}
+
+interface WorkoutCardProps {
+  title: string;
+  subtitle: string;
+  description: string;
+}
