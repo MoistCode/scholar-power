68778bd264ea14442e5dcfabb2407e6c229cea9e
moistcode <tpham@smugmug.com>
2023-02-04 11:20:37 -0800

    run token logic within useeffect

--- src/hooks/useEnsureLoggedIn.tsx
+++ src/hooks/useEnsureLoggedIn.tsx
@@ -1,21 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import jwt_decode from 'jwt-decode';
 
 
 // Determines whether or not a user is considered to be logged in.
 export function useEnsureLoggedIn() {
-  let userToken = localStorage.getItem('user_token');
+  let [ token, setToken ] = useState<string>();
 
-  if (userToken) {
-    let decodedToken = jwt_decode(userToken);
-    console.log('cowman123', decodedToken);
+  useEffect(() => {
+    let userToken = localStorage.getItem('user_token');
+  
+    if (userToken) {
+      let decodedToken = jwt_decode(userToken) as string;
+      console.log('cowman123', decodedToken);
+  
+      // if (decodedToken && (Date.now() >= decodedToken?.exp * 1000)) {
+  
+      // }    
 
-    // if (decodedToken && (Date.now() >= decodedToken?.exp * 1000)) {
+      setToken(decodedToken);
+    }
+  }, [])
 
-    // }    
-  }
-
-  let [ token, setToken ] = useState(userToken);
+  
   console.log('cowman useEnsureLoggedIn', token);
   return {
     isLoggedIn: Boolean(token),
