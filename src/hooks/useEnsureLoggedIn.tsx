165caae796f55268ffda77bcef3796522153fc63
moistcode <tpham@smugmug.com>
2023-02-04 11:13:44 -0800

    testing token setting

--- src/hooks/useEnsureLoggedIn.tsx
+++ src/hooks/useEnsureLoggedIn.tsx
@@ -9,13 +9,14 @@ export function useEnsureLoggedIn() {
   if (userToken) {
     let decodedToken = jwt_decode(userToken);
     console.log('cowman123', decodedToken);
+
     // if (decodedToken && (Date.now() >= decodedToken?.exp * 1000)) {
 
     // }    
   }
 
   let [ token, setToken ] = useState(userToken);
-
+  console.log('cowman useEnsureLoggedIn', token);
   return {
     isLoggedIn: Boolean(token),
     setToken,
